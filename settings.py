ccf6d4cfb97d2b744300647072827bdd582cbe99
Ukang'a Dickson <ukanga@gmail.com>
2012-05-28 16:05:04 +0300

    DHIS2 settings

--- settings.py
+++ settings.py
@@ -11,12 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
     }
 }
 
@@ -116,9 +112,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'south',
+    'django_extensions',
+    'main',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -143,3 +142,7 @@ LOGGING = {
         },
     }
 }
+
+DHIS2_DATA_VALUE_SET_URL = "http://apps.dhis2.org/demo/api/dataValueSets"
+DHIS2_USERNAME = "admin"
+DHIS2_PASSWORD = "district"
