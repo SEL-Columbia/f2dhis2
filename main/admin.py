4889513aadd625b6734b6db6c891215c42b22786
Ukang'a Dickson <ukanga@gmail.com>
2012-06-09 15:41:02 +0300

    show models in admin

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from main.models import DataElement, DataSet, DataValueSet, FormDataElement, FormhubService
+from main.models import DataElement, DataSet, DataValueSet, FormDataElement, FormhubService, DataQueue, OrganizationUnit
 
 admin.site.register(DataElement)
 admin.site.register(DataSet)
 admin.site.register(DataValueSet)
 admin.site.register(FormDataElement)
 admin.site.register(FormhubService)
+admin.site.register(OrganizationUnit)
+admin.site.register(DataQueue)
