5e0fd7369c8502f497a355052581da2a97a1bef5
Ukang'a Dickson <ukanga@gmail.com>
2012-07-03 11:34:48 +0300

    remove orgunit as part of DataValueSet model

--- main/models.py
+++ main/models.py
@@ -91,7 +91,6 @@ class DataValueSet(models.Model):
         verbose_name_plural = _(u"Data Value Sets")
 
     service = models.ForeignKey(FormhubService, verbose_name=_(u"Formhub Service"))
-    organization_unit = models.ForeignKey(OrganizationUnit)
     data_set = models.ForeignKey(DataSet, verbose_name=_(u"Data Set"))
     created_on = models.DateTimeField(_(u"Created on"), auto_now_add=True)
     modified_on = models.DateTimeField(_(u"Modified on"), auto_now=True)
