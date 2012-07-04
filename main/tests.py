cc34356584463c9236bd3d2a5eaf50014aa1ed6b
Ukang'a Dickson <ukanga@gmail.com>
2012-07-04 13:38:14 +0300

    test helper function to import dhis2 datasets

--- main/tests.py
+++ main/tests.py
@@ -13,6 +13,7 @@ class Main(TestCase):
         self.client = Client()
         self.base_url = 'http://testserver'
         self.ds_url = u'http://apps.dhis2.org/demo/api/dataSets/pBOMPrpg1QX'
+        self.ds_import_url = reverse(views.dataset_import)
 
     def _create_user(self, username, password):
         user, created = User.objects.get_or_create(username=username)
@@ -25,25 +26,26 @@ class Main(TestCase):
         assert client.login(username=username, password=password)
         return client
 
+    def _import_dataset(self):
+        post_data = {'data_set_url': self.ds_url}
+        response = self.client.post(self.ds_import_url, post_data)
+        self.assertEqual(response.status_code, 200)
 
     def test_index_page(self):
         response = self.client.get(reverse(views.main))
         self.assertEqual(response.status_code, 200)
 
     def test_import_dataset(self):
-        ds_import_url = reverse(views.dataset_import)
-        response = self.client.get(ds_import_url)
+        response = self.client.get(self.ds_import_url)
         # need to login first, should redirect
         self.assertEqual(response.status_code, 302)
         self.user = self._create_user(self.username, self.password)
         self.client = self._login(self.username, self.password)
         # should be successful this time
-        response = self.client.get(ds_import_url)
+        response = self.client.get(self.ds_import_url)
         self.assertEqual(response.status_code, 200)
         # check saved Dataset
         count = DataSet.objects.count()
         self.assertEqual(count, 0)
-        post_data = {'data_set_url': self.ds_url}
-        response = self.client.post(ds_import_url, post_data)
-        self.assertEqual(response.status_code, 200)
+        self._import_dataset()
         self.assertEqual(DataSet.objects.count(), count + 1)
