27bacdd45cc42e9cf0c71d8c83206c52674cfcbe
Ukang'a Dickson <ukanga@gmail.com>
2012-07-04 12:50:41 +0300

    test access to main page

--- main/tests.py
+++ main/tests.py
@@ -1,16 +1,14 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
+from main import views
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class Main(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.base_url = 'http://testserver'
+
+    def test_index_page(self):
+        response = self.client.get(reverse(views.main))
+        self.assertEqual(response.status_code, 200)
