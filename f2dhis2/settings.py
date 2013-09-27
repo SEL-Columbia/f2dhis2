b26856e2cb6dca53f9cf7fe321eacf8597b0886e
Larry Weya <larryweya@gmail.com>
2013-09-27 12:06:52 +0300

    added static root to settings

--- f2dhis2/settings.py
+++ f2dhis2/settings.py
@@ -1,7 +1,14 @@
+import os
+
 # Django settings for f2dhis2 project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+CURRENT_FILE = os.path.abspath(__file__)
+PROJECT_ROOT = os.path.realpath(
+    os.path.join(os.path.dirname(CURRENT_FILE), '..'))
+PRINT_EXCEPTION = False
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -52,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
